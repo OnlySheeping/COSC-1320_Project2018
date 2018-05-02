a264e1f17233482437ea943828b9b42702192beb
programmer <programmer@BB21502>
2018-05-02 18:59:39 -0500

    new update

--- ProjectFinalExam1/Participant.Designer.cs
+++ ProjectFinalExam1/Participant.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Participant));
             this.label1 = new System.Windows.Forms.Label();
             this.lblPassword = new System.Windows.Forms.Label();
             this.tbxPassword = new System.Windows.Forms.TextBox();
@@ -37,12 +38,15 @@
             this.tbxLastName = new System.Windows.Forms.TextBox();
             this.lblParticipantAge = new System.Windows.Forms.Label();
             this.tbxParticipantAge = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(230, 9);
+            this.label1.Location = new System.Drawing.Point(336, 217);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(108, 13);
             this.label1.TabIndex = 0;
@@ -51,7 +55,7 @@
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(115, 61);
+            this.lblPassword.Location = new System.Drawing.Point(230, 269);
             this.lblPassword.Name = "lblPassword";
             this.lblPassword.Size = new System.Drawing.Size(53, 13);
             this.lblPassword.TabIndex = 1;
@@ -60,7 +64,7 @@
             // 
             // tbxPassword
             // 
-            this.tbxPassword.Location = new System.Drawing.Point(192, 58);
+            this.tbxPassword.Location = new System.Drawing.Point(303, 262);
             this.tbxPassword.Name = "tbxPassword";
             this.tbxPassword.Size = new System.Drawing.Size(180, 20);
             this.tbxPassword.TabIndex = 2;
@@ -69,7 +73,7 @@
             // lblParticipantFirstName
             // 
             this.lblParticipantFirstName.AutoSize = true;
-            this.lblParticipantFirstName.Location = new System.Drawing.Point(12, 103);
+            this.lblParticipantFirstName.Location = new System.Drawing.Point(226, 328);
             this.lblParticipantFirstName.Name = "lblParticipantFirstName";
             this.lblParticipantFirstName.Size = new System.Drawing.Size(57, 13);
             this.lblParticipantFirstName.TabIndex = 3;
@@ -77,7 +81,7 @@
             // 
             // tbxParticipantFirstName
             // 
-            this.tbxParticipantFirstName.Location = new System.Drawing.Point(75, 100);
+            this.tbxParticipantFirstName.Location = new System.Drawing.Point(303, 321);
             this.tbxParticipantFirstName.Name = "tbxParticipantFirstName";
             this.tbxParticipantFirstName.Size = new System.Drawing.Size(177, 20);
             this.tbxParticipantFirstName.TabIndex = 4;
@@ -86,7 +90,7 @@
             // lblParticipantLastName
             // 
             this.lblParticipantLastName.AutoSize = true;
-            this.lblParticipantLastName.Location = new System.Drawing.Point(12, 143);
+            this.lblParticipantLastName.Location = new System.Drawing.Point(221, 374);
             this.lblParticipantLastName.Name = "lblParticipantLastName";
             this.lblParticipantLastName.Size = new System.Drawing.Size(58, 13);
             this.lblParticipantLastName.TabIndex = 5;
@@ -94,7 +98,7 @@
             // 
             // tbxLastName
             // 
-            this.tbxLastName.Location = new System.Drawing.Point(76, 143);
+            this.tbxLastName.Location = new System.Drawing.Point(303, 367);
             this.tbxLastName.Name = "tbxLastName";
             this.tbxLastName.Size = new System.Drawing.Size(175, 20);
             this.tbxLastName.TabIndex = 6;
@@ -102,7 +106,7 @@
             // lblParticipantAge
             // 
             this.lblParticipantAge.AutoSize = true;
-            this.lblParticipantAge.Location = new System.Drawing.Point(43, 190);
+            this.lblParticipantAge.Location = new System.Drawing.Point(253, 429);
             this.lblParticipantAge.Name = "lblParticipantAge";
             this.lblParticipantAge.Size = new System.Drawing.Size(26, 13);
             this.lblParticipantAge.TabIndex = 7;
@@ -110,16 +114,46 @@
             // 
             // tbxParticipantAge
             // 
-            this.tbxParticipantAge.Location = new System.Drawing.Point(76, 187);
+            this.tbxParticipantAge.Location = new System.Drawing.Point(303, 422);
             this.tbxParticipantAge.Name = "tbxParticipantAge";
             this.tbxParticipantAge.Size = new System.Drawing.Size(100, 20);
             this.tbxParticipantAge.TabIndex = 8;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(29, 70);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(738, 119);
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(29, 9);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(738, 57);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Participant";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Participant
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(579, 309);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(781, 507);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.tbxParticipantAge);
             this.Controls.Add(this.lblParticipantAge);
             this.Controls.Add(this.tbxLastName);
@@ -131,6 +165,8 @@
             this.Controls.Add(this.label1);
             this.Name = "Participant";
             this.Text = "Participant";
+            this.Load += new System.EventHandler(this.Participant_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -147,5 +183,7 @@
         private System.Windows.Forms.TextBox tbxLastName;
         private System.Windows.Forms.Label lblParticipantAge;
         private System.Windows.Forms.TextBox tbxParticipantAge;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
     }
 }
