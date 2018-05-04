dc35f62d548ce37bbb4c9e113237a28e738c0b72
programmer <programmer@BB21501>
2018-05-04 16:56:02 -0500

    added asterisks to password textbox deleted extra code from LogOn

--- ProjectFinalExam1/LogOn.cs
+++ ProjectFinalExam1/LogOn.cs
@@ -16,17 +16,10 @@ namespace ProjectFinalExam1
 {
     public partial class LogOn : Form
     {
-        //SqlConnection connection = new SqlConnection();
-        string userName;
-        string password;
-        string firstName;
-        string lastName;
-        int userAge;
-        int roleID;
-
         public LogOn()
         {
             InitializeComponent();
+            tbPassword.PasswordChar = '*';
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -49,71 +42,15 @@ namespace ProjectFinalExam1
 
         }
 
-        //public class UserData
-        //{
-        //    //static SqlConnection conn = new System.Data.SqlClient.SqlConnection(@"Server=cis1.actx.edu;Database=Project1;User Id=db1;Password = db10");
-        //    //SqlConnection conn = Program.conn;
-        //    SqlCommand cmd = new System.Data.SqlClient.SqlCommand("select * from Project1.dbo.Users", Program.conn);
-
-        //    SqlDataReader dr;
-            
-
-
-
-        //    //  if (userArray[5] == "1")
-        //    //   {
-        //    //       new AdminMenu().Show();
-        //    //   }
-
-        //    // try
-        //    // {
-
-        //    //    connString = "Server=cis1.actx.edu;Database=Project1;User Id=db1;Password = db10;";
-        //    //        pubConnection.ConnectionString = connString;
-        //    //            pubConnection.Open();
-        //    //            pubcommand = new SqlCommand();
-        //    //    pubcommand.Connection = pubConnection;
-        //    //            pubcommand.CommandText = new SqlCommand("Select * from Project1.dbo.Users", )
-        //    //            dr = pubcommand.ExecuteReader();
-
-
-
-
-        //    //            while (userDataReader.Read())
-        //    //            {
-        //    //                userArray.Add(userDataReader["Username{0}, Passsword, UserFirstName, UserLastName, UserAge, RoleID"]);
-
-        //    //                foreach ()
-
-        //    //            }
-        //    //userArg[0] = userArray[1][5];
-        //    //        return userArray;
-        //    //        }
-        //    //        catch (SqlException ex)
-        //    //        {
-        //    //   throw ex;
-        //    //}
-        //    //finally
-        //    //{
-        //    //   if (pubConnection != null)
-        //    //  {
-        //    //     pubConnection.Close();
-        //    //}
-        //    //  }
-
-        //}
-
         private void button1_Click(object sender, EventArgs e)
         {
 
             using (SqlCommand command = new SqlCommand("SELECT * FROM dbo.Users where Username =@username and Passsword = @password", Program.conn))
             {
-                //
                 // Invoke ExecuteReader method.
-                //
+
                 command.Parameters.AddWithValue("@username", tbUserName.Text);
                 command.Parameters.AddWithValue("@password", tbPassword.Text);
-
 
                 SqlDataReader reader = command.ExecuteReader();
                 if (reader.HasRows)
@@ -147,8 +84,6 @@ namespace ProjectFinalExam1
                             MessageBox.Show("Please enter correct Username and Password");
                         }
                     }
-
-
                 }
                 else
                 {
