1c515ccd4ef460cbf85c30f13762e60a75d3cf2b
Loretta <Loretta@DESKTOP-RQQHP4M>
2018-05-03 20:15:16 -0500

    Worked on the Manage event pages

--- ProjectFinalExam1/ManageEvents.Designer.cs
+++ ProjectFinalExam1/ManageEvents.Designer.cs
@@ -43,10 +43,10 @@
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(126, 71);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.pictureBox1.Location = new System.Drawing.Point(168, 87);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(738, 119);
+            this.pictureBox1.Size = new System.Drawing.Size(984, 146);
             this.pictureBox1.TabIndex = 14;
             this.pictureBox1.TabStop = false;
             // 
@@ -56,19 +56,20 @@
             this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(126, 9);
-            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Location = new System.Drawing.Point(168, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(738, 57);
+            this.label1.Size = new System.Drawing.Size(984, 70);
             this.label1.TabIndex = 13;
             this.label1.Text = " Event Manager";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // btn1
             // 
-            this.btn1.Location = new System.Drawing.Point(126, 219);
+            this.btn1.Location = new System.Drawing.Point(168, 270);
+            this.btn1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn1.Name = "btn1";
-            this.btn1.Size = new System.Drawing.Size(142, 37);
+            this.btn1.Size = new System.Drawing.Size(189, 46);
             this.btn1.TabIndex = 15;
             this.btn1.Text = "ADD";
             this.btn1.UseVisualStyleBackColor = true;
@@ -76,43 +77,49 @@
             // 
             // btn2
             // 
-            this.btn2.Location = new System.Drawing.Point(126, 328);
+            this.btn2.Location = new System.Drawing.Point(168, 404);
+            this.btn2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn2.Name = "btn2";
-            this.btn2.Size = new System.Drawing.Size(142, 38);
+            this.btn2.Size = new System.Drawing.Size(189, 47);
             this.btn2.TabIndex = 16;
             this.btn2.Text = "DELETE";
             this.btn2.UseVisualStyleBackColor = true;
             // 
             // btn3
             // 
-            this.btn3.Location = new System.Drawing.Point(126, 271);
+            this.btn3.Location = new System.Drawing.Point(168, 334);
+            this.btn3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn3.Name = "btn3";
-            this.btn3.Size = new System.Drawing.Size(142, 38);
+            this.btn3.Size = new System.Drawing.Size(189, 47);
             this.btn3.TabIndex = 17;
             this.btn3.Text = "EDIT";
             this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(412, 207);
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(549, 255);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(452, 368);
+            this.listBox1.Size = new System.Drawing.Size(601, 452);
             this.listBox1.TabIndex = 18;
             // 
             // ManageEvents
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(970, 873);
+            this.ClientSize = new System.Drawing.Size(1293, 1055);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.btn3);
             this.Controls.Add(this.btn2);
             this.Controls.Add(this.btn1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "ManageEvents";
             this.Text = "Event Manager";
             this.Load += new System.EventHandler(this.Form3_Load);
