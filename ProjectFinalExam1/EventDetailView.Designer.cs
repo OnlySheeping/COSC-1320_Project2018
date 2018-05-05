ef859ef11dd7c458a26f37bb7f2d10630ca722b0
Diane <Diane@LAPTOP-LK5MJIOS>
2018-05-05 16:21:44 -0500

    add button

--- ProjectFinalExam1/EventDetailView.Designer.cs
+++ ProjectFinalExam1/EventDetailView.Designer.cs
@@ -31,6 +31,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fmdetailView));
             this.lblDetailBanner = new System.Windows.Forms.Label();
             this.lvDetailView = new System.Windows.Forms.ListView();
+            this.btnPopulate = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblDetailBanner
@@ -60,19 +61,29 @@
             this.lvDetailView.View = System.Windows.Forms.View.Details;
             this.lvDetailView.SelectedIndexChanged += new System.EventHandler(this.lvDetailView_SelectedIndexChanged);
             // 
+            // btnPopulate
+            // 
+            this.btnPopulate.Location = new System.Drawing.Point(268, 592);
+            this.btnPopulate.Name = "btnPopulate";
+            this.btnPopulate.Size = new System.Drawing.Size(111, 42);
+            this.btnPopulate.TabIndex = 15;
+            this.btnPopulate.Text = "Populate";
+            this.btnPopulate.UseVisualStyleBackColor = true;
+            // 
             // fmdetailView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(694, 749);
+            this.Controls.Add(this.btnPopulate);
             this.Controls.Add(this.lvDetailView);
             this.Controls.Add(this.lblDetailBanner);
             this.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "fmdetailView";
-            this.ShowIcon = false;
             this.Text = "Event Detail View";
             this.Load += new System.EventHandler(this.EventDetailView_Load);
             this.ResumeLayout(false);
@@ -82,5 +93,6 @@
         #endregion
         private System.Windows.Forms.Label lblDetailBanner;
         private System.Windows.Forms.ListView lvDetailView;
+        private System.Windows.Forms.Button btnPopulate;
     }
 }
