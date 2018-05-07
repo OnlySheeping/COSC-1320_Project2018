cdd7516105de7ca1da4c5b4f9cb72b89aeed79ce
programmer <programmer@BB21501>
2018-05-06 19:54:25 -0500

    Created CurrentUser class and then created VerifyUserNamePassword method, VerifyFirstName method, VerifyLastName method for it. Instantiated class in LogOn form and called the methods to login and show First Name and Last Name to user after log in in Message Box.

--- EventSystem/EventSystem/User.cs
+++ EventSystem/EventSystem/User.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using System.Data.SqlClient;
 using System.Data.Sql;
 using System.Windows.Forms;
@@ -12,12 +11,9 @@ using System.Windows.Input;
 
 namespace EventSystem
 {
-    // included EventAdmin, Participant for logon transitioning to each of those pages
-    public abstract class User //: //EventAdmin, Participant
+    public abstract class User
     {
         SqlConnection connection = new SqlConnection();
-        string[] user = new string[4];
-        int[] id = new int[3];
 
         private string firstName
         {
@@ -91,7 +87,7 @@ namespace EventSystem
             }
         }
 
-        private string username
+        public string username
         {
             get
             {
@@ -114,58 +110,6 @@ namespace EventSystem
             {
             }
         }
+        public int roleID { get; set; }
     }
-    //public class UserData
-    //{
-    //    public void GetLogin(string userName, string password)
-    //    { 
-    //    //Invoke ExecuteReader method.
-    //        using (SqlCommand command = new SqlCommand("SELECT * FROM dbo.Users where Username =@username and Passsword = @password", Program.conn))
-    //        {
-    //            command.Parameters.AddWithValue("@username", userName);
-    //            command.Parameters.AddWithValue("@password", password);
-    //        }
-    //        SqlDataReader reader = command.ExecuteReader();
-
-    //        if (reader.HasRows)
-    //        {
-    //            //UserData();
-    //            while (reader.Read())
-    //            {
-    //                string userName1 = reader.GetString(reader.GetOrdinal("Username"));
-    //                string firstName = reader.GetString(reader.GetOrdinal("UserFirstName"));
-    //                string lastName = reader.GetString(reader.GetOrdinal("UserLastName"));
-    //                int userAge = reader.GetInt32(reader.GetOrdinal("UserAge"));
-    //                int roleID = reader.GetInt32(reader.GetOrdinal("RoleID"));
-
-    //                if (roleID == 1)
-    //                {
-    //                    MessageBox.Show("Logon Successful" + Environment.NewLine + "Welcome" + " " + firstName + " " + lastName);
-    //                    ProjectFinalExam1.ParticipantMenu participant = new ParticipantMenu();
-    //                    participant.Show();
-    //                    Visible = false;
-    //                }
-    //                else if (roleID == 2)
-    //                {
-    //                    MessageBox.Show("Logon Successful" + Environment.NewLine + "Welcome" + " " + firstName + " " + lastName);
-    //                    AdminMenu admin = new AdminMenu();
-    //                    admin.Show();
-    //                    Visible = false;
-    //                }
-    //                else
-    //                {
-
-    //                    MessageBox.Show("Please enter correct Username and Password");
-    //                }
-    //            }
-    //        }
-    //        else
-    //        {
-    //            MessageBox.Show("Contact Administrator");
-    //        }
-
-        }
-//    }
-//}
-
-//    }
+}
