5ccb644124a2a5941ebd0c1e148a11c30808a2c9
programmer <programmer@BB21502>
2018-05-07 19:23:47 -0500

    Revert "Added all the fields we needed on the Create New Participant form. Started to code the buttons, but did not finish."
    This reverts commit 45402baa7dfd9c0d3196cee7c9c29822a0b7ae6f.

--- ProjectFinalExam1/CreateNewParticipant.cs
+++ ProjectFinalExam1/CreateNewParticipant.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace EventSystem
 {
-    public partial class CreateNewParticipant : Form
+    public partial class frmCreateNewParticipant : Form
     {
-        public CreateNewParticipant()
+        public frmCreateNewParticipant()
         {
             InitializeComponent();
         }
@@ -20,32 +20,6 @@ namespace EventSystem
         private void lstbxCreateNewParticipant_SelectedIndexChanged(object sender, EventArgs e)
         {
 
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void frmCreateNewParticipant_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Are you sure you want to add this participant to the database?");
-        }
-
-        private void btnCancel_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Are you sure you would like to cancel? ");
-
-        }
-
-        private void btnBack_Click(object sender, EventArgs e)
-        {
-            
         }
     }
 }
