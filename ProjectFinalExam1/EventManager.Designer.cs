f1cd9efad9bf89b76f1a4423fa90e281ff82d0e2
programmer <programmer@BB21501>
2018-05-09 12:42:37 -0500

    creating links from buttons to forms

--- ProjectFinalExam1/EventManager.Designer.cs
+++ ProjectFinalExam1/EventManager.Designer.cs
@@ -28,13 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnCreateEvents = new System.Windows.Forms.Button();
+            this.btnAllEvents = new System.Windows.Forms.Button();
+            this.pnlHeader = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
             this.SuspendLayout();
+            // 
+            // btnCreateEvents
+            // 
+            this.btnCreateEvents.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnCreateEvents.Location = new System.Drawing.Point(233, 248);
+            this.btnCreateEvents.Name = "btnCreateEvents";
+            this.btnCreateEvents.Size = new System.Drawing.Size(150, 50);
+            this.btnCreateEvents.TabIndex = 0;
+            this.btnCreateEvents.Text = "Create Events";
+            this.btnCreateEvents.UseVisualStyleBackColor = false;
+            this.btnCreateEvents.Click += new System.EventHandler(this.btnCreateEvents_Click);
+            // 
+            // btnAllEvents
+            // 
+            this.btnAllEvents.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnAllEvents.Location = new System.Drawing.Point(389, 248);
+            this.btnAllEvents.Name = "btnAllEvents";
+            this.btnAllEvents.Size = new System.Drawing.Size(150, 50);
+            this.btnAllEvents.TabIndex = 1;
+            this.btnAllEvents.Text = "All Events";
+            this.btnAllEvents.UseVisualStyleBackColor = false;
+            this.btnAllEvents.Click += new System.EventHandler(this.btnAllEvents_Click);
+            // 
+            // pnlHeader
+            // 
+            this.pnlHeader.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pnlHeader.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pnlHeader.Location = new System.Drawing.Point(12, 12);
+            this.pnlHeader.Name = "pnlHeader";
+            this.pnlHeader.Size = new System.Drawing.Size(763, 100);
+            this.pnlHeader.TabIndex = 2;
+            this.pnlHeader.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlHeader_Paint);
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.flowLayoutPanel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(12, 482);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(763, 100);
+            this.flowLayoutPanel2.TabIndex = 3;
             // 
             // EventManager
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(787, 594);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.pnlHeader);
+            this.Controls.Add(this.btnAllEvents);
+            this.Controls.Add(this.btnCreateEvents);
             this.Name = "EventManager";
             this.Text = "EventManager";
             this.Load += new System.EventHandler(this.EventManager_Load);
@@ -43,5 +93,10 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCreateEvents;
+        private System.Windows.Forms.Button btnAllEvents;
+        private System.Windows.Forms.FlowLayoutPanel pnlHeader;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
     }
 }
